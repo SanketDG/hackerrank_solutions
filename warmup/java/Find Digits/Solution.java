c8922204eec687f073a8264f775c8a94eeaa340e
SanketDG <sanketdasgupta@gmail.com>
2015-01-05 23:37:09 +0530

    Change dir structure

--- warmup/java/Find Digits/Solution.java
+++ warmup/java/Find Digits/Solution.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class find_digits{
+public class Solution{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int T = in.nextInt();
