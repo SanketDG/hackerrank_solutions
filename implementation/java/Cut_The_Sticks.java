15d44bf87a424505bd22280d59cff6b68e37c5b1
SanketDG <sanketdasgupta@gmail.com>
2015-01-25 01:13:19 +0530

    Use ArrayList instead of LinkedList

--- implementation/java/Cut_The_Sticks.java
+++ implementation/java/Cut_The_Sticks.java
@@ -8,7 +8,7 @@ public class Cut_The_Sticks {
         int n = sc.nextInt();
 
         int lengths[] = new int[n];
-        List<Integer> output = new LinkedList<Integer>();
+        List<Integer> output = new ArrayList<Integer>();
 
         for (int i = 0; i < n; i++)
             lengths[i] = sc.nextInt();
@@ -17,16 +17,14 @@ public class Cut_The_Sticks {
         Arrays.sort(lengths);
 
         for (int i = 0; i < n; i++) {
-            if (lengths[i] == 0)
+            if (lengths[i] <= 0)
                 continue;
             else {
                 output.add(n - i);
                 final int  v = lengths[i];
-                for (int j = i; j < n; j++) {    // This loop isn't working like it should
-                    lengths[j] -= lengths[i];
-                 // System.out.print(lengths[j] + " ");  // For debugging purposes
+                for (int j = i; j < n; j++) {
+                    lengths[j] -= v;
                 }
-             // System.out.println("");
             }
         }
 
