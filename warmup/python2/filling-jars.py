0342a74926640c41753869606c18dd02e35b85a0
SanketDG <sanketdasgupta@gmail.com>
2015-01-25 19:58:23 +0530

    Fixed solution

--- warmup/python2/filling-jars.py
+++ warmup/python2/filling-jars.py
@@ -1,8 +1,6 @@
 n, m = map(int, raw_input().split())
-jars = [0] * n
 sum = 0
 for i in range(m):
     a, b, k = map(int, raw_input().split())
-    for j in range(a, b + 1):
-        sum += (b-a+1)*k
-print sum(jars) / n
+    sum += (b-a+1)*k
+print sum / n
