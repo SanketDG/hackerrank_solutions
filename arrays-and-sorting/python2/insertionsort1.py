00a2bef32cfaa144e5c57551593e5befa78d85a4
SanketDG <sanketdasgupta@gmail.com>
2015-02-02 00:05:14 +0530

    update solution

--- arrays-and-sorting/python2/insertionsort1.py
+++ arrays-and-sorting/python2/insertionsort1.py
@@ -1,14 +1,15 @@
 n = int(raw_input())
 ar = [int(x) for x in raw_input().split()]
 l = ar[n - 1]
+j = 0
 for i in range(n - 1, 0, -1):
     if ar[i - 1] > l:
         ar[i] = ar[i - 1]
         print " ".join(map(str, ar))
-        print i
     else:
         ar[i] = l
-        print " ".join(map(str, ar))
+        j = 1
         break
-ar[0] = l
+if not j:
+    ar[0] = l
 print " ".join(map(str, ar))
