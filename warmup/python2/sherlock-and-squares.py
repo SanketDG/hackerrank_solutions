1ef49d4889482662bf5e80e7350d598099aef3c1
SanketDG <sanketdasgupta@gmail.com>
2015-02-02 22:33:13 +0530

    update sherlock and squares solution

--- warmup/python2/sherlock-and-squares.py
+++ warmup/python2/sherlock-and-squares.py
@@ -1,10 +1,5 @@
 from math import sqrt, floor, ceil
 
-
-def isperfectsquare(n):
-    return True if sqrt(n).is_integer() else False
-
 for _ in xrange(int(raw_input())):
     a, b = map(int, raw_input().split())
-    c = 0
     print int(floor(sqrt(b)) - ceil(sqrt(a)) + 1)
