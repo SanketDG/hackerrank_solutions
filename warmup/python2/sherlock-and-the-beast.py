9aecdfac876f6039df4eacff9e966df12123946a
SanketDG <sanketdasgupta@gmail.com>
2015-02-17 23:53:22 +0530

    add sherlock and the beast solution

---
+++ warmup/python2/sherlock-and-the-beast.py
@@ -0,0 +1,17 @@
+def getPivot(n):
+    while(n > 0):
+        if not n % 3:
+            break
+        else:
+            n -= 5
+
+    return n
+
+for _ in xrange(int(raw_input())):
+    n = int(raw_input())
+    n1 = n
+    pivot = getPivot(n)
+    if pivot < 0:
+        print "-1"
+        continue
+    print "5" * pivot + "3" * (n1 - pivot)
